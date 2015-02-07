dc023c6908eb28e0b2f94893676b798c1aa33222
Ksenia Smith <smithkseniav@gmail.com>
2015-02-06 16:32:13 -0800

    Made getWinner function.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -20,13 +20,11 @@ function randomPlay() {
 ////////////////////////////////////////////////
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
-//done 02/04/15
 function getPlayerMove(move) {
     // If a move is given, your expression should evaluate to that move.
     // If move is not specified / is null, your expression should run getInput()
     return move || getInput();
 }
-//done 02/04/15
 function getComputerMove(move) {
     // If a move is given, your expression should evaluate to that move.
     // If move is not specified / is null, your expression should run randomPlay()
@@ -37,7 +35,23 @@ function getWinner(playerMove,computerMove) {
     // This function should either give us back 'player', 'computer', or 'tie'.
     // The rules of the game are that rock beats scissors, scissors beats paper, and paper beats rock.
     // Assume that the only possible input values we can get are 'rock', 'paper', and 'scissors'.
+    playerMove = getPlayerMove();
+    console.log("Your move is: " + playerMove);
+    computerMove = getComputerMove();
+    console.log("Computer move is: " + computerMove);
 
+    if (playerMove === computerMove){
+        return "It's a tie!";
+    }
+    else if (playerMove === "rock"){
+        return computerMove === "scissors"? "Player wins!" : "Computer wins!";
+    }
+     else if (playerMove === "paper"){
+        return computerMove === "rock"? "Player wins!" : "Computer wins!";
+    }
+     else if (playerMove === "scissors"){
+        return computerMove === "paper"? "Player wins!" : "Computer wins!";
+    } 
 }
 
 function playToFive() {
